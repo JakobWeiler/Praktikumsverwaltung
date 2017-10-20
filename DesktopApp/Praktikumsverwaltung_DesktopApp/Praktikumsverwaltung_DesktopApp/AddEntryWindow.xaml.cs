a99cee5c2d93a465c20efd7899a813c4bd1848c1
LorenzFritz <lorenz332@gmail.com>
2017-10-20 11:17:39 +0200

    AddEntry: checks if values are correct

--- DesktopApp/Praktikumsverwaltung_DesktopApp/Praktikumsverwaltung_DesktopApp/AddEntryWindow.xaml.cs
+++ DesktopApp/Praktikumsverwaltung_DesktopApp/Praktikumsverwaltung_DesktopApp/AddEntryWindow.xaml.cs
@@ -23,5 +23,95 @@ namespace Praktikumsverwaltung_DesktopApp
         {
             InitializeComponent();
         }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            String title = null, description = null, formOfIntern = null, dpStart = null, dpEnd = null;
+            int salary = -1;
+
+            try
+            {
+                lblErrorTitle.Foreground = Brushes.Red;
+                lblErrorDescription.Foreground = Brushes.Red;
+                lblErrorSalary.Foreground = Brushes.Red;
+                lblErrorStartDate.Foreground = Brushes.Red;
+                lblErrorEndDate.Foreground = Brushes.Red;
+                lblErrorFormOfIntern.Foreground = Brushes.Red;
+
+                if (txtTitle.Text.Length > 0)
+                {
+                    title = txtTitle.Text;
+                    lblErrorTitle.Content = "";
+                }
+                else
+                {
+                    lblErrorTitle.Content = "mind. 1 letter";
+                }
+
+                if (txtDescription.Text.Length > 0)
+                {
+                    description = txtDescription.Text;
+                    lblErrorDescription.Content = "";
+                }
+                else
+                {
+                    lblErrorDescription.Content = "mind. 1 letter";
+                }
+                
+                if (txtSalary.Text.Length > 0)
+                {
+                    // checks if salary doesn't consist of letters
+                    int.TryParse(txtSalary.Text.ToString(), out salary);        // returns the salary (with type int) or 0 if parse isn't possible
+
+                    if (salary > 0 || txtSalary.Text.Equals("0"))       // we have to check if the value is 0, in order to differ from the result of tryParse (Because the user could type in 0).
+                    {
+                        salary = int.Parse(txtSalary.Text);
+                        lblErrorSalary.Content = "";
+                    }
+                    else
+                    {
+                        lblErrorSalary.Content = "type in a number (no letters)";
+                    }
+                }
+                else
+                {
+                    lblErrorSalary.Content = "mind. 1 digit";
+                }
+
+                if (datePickerStartDate.Text.Length > 0)
+                {
+                    dpStart = datePickerStartDate.Text;
+                    lblErrorStartDate.Content = "";
+                }
+                else
+                {
+                    lblErrorStartDate.Content = "select a date";
+                }
+
+                if (datePickerEndDate.Text.Length > 0)
+                {
+                    dpEnd = datePickerEndDate.Text;
+                    lblErrorEndDate.Content = "";
+                }
+                else
+                {
+                    lblErrorEndDate.Content = "select a date";
+                }
+
+                if (txtFormOfIntern.Text.Length > 0)
+                {
+                    formOfIntern = txtFormOfIntern.Text;
+                    lblErrorFormOfIntern.Content = "";
+                }
+                else
+                {
+                    lblErrorFormOfIntern.Content = "mind. 1 letter";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
     }
 }
