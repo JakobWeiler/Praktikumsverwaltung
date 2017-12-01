216893a4fb5a5363b1e8491902b50a3a67f898ab
schueler <schueler@ubuntu>
2017-12-01 03:14:35 -0800

    Webservice initial commit

---
+++ PraktikumsverwaltungWebService/src/java/pkgData/Company.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgData;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import org.bson.types.ObjectId;
+
+/**
+ *
+ * @author schueler
+ */
+@XmlRootElement
+public class Company {
+    private ObjectId id;
+    private String name;
+    private String location;
+    private int numberOfEmployees;
+    private String contactPerson;
+
+    public Company() {
+        
+    }
+    
+    public Company(ObjectId id, String name, String location, int numberOfEmployees, String contactPerson) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.numberOfEmployees = numberOfEmployees;
+        this.contactPerson = contactPerson;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getNumberOfEmployees() {
+        return numberOfEmployees;
+    }
+
+    public void setNumberOfEmployees(int numberOfEmployees) {
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    public String getContactPerson() {
+        return contactPerson;
+    }
+
+    public void setContactPerson(String contactPerson) {
+        this.contactPerson = contactPerson;
+    }
+}
