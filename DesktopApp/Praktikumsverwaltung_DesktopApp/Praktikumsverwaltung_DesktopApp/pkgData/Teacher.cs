7da07d801084b5eeb09821273b44d36820aa4d1b
LorenzFritz <lorenz332@gmail.com>
2018-01-12 15:45:20 +0100

    adaption of several classes to the webservice.

---
+++ DesktopApp/Praktikumsverwaltung_DesktopApp/Praktikumsverwaltung_DesktopApp/pkgData/Teacher.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktikumsverwaltung_DesktopApp.pkgData
+{
+    class Teacher : Person
+    {
+        public bool IsAdmin { get; set; }
+
+        public Teacher(string id, string firstname, string lastname, string email, string username, string password, bool isActive, bool isAdmin)
+        : base(id, firstname, lastname, email, username, password, isActive)
+        {
+            IsAdmin = isAdmin;
+        }
+    }
+}
