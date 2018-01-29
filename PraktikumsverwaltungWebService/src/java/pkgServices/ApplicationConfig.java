2d4e600eebd33b63a8dc65fd19c1c3cdc9c5de2f
JakobWeiler <jakob.weiler@edu.htl-villach.at>
2018-01-29 02:59:47 -0800

    EntryWebResource added

--- PraktikumsverwaltungWebService/src/java/pkgServices/ApplicationConfig.java
+++ PraktikumsverwaltungWebService/src/java/pkgServices/ApplicationConfig.java
@@ -33,9 +33,9 @@ public class ApplicationConfig extends Application {
         resources.add(pkgServices.CompanyResource.class);
         resources.add(pkgServices.DepartmentResource.class);
         resources.add(pkgServices.EntryAdminResource.class);
-        resources.add(pkgServices.EntryAndroidResource.class);
         resources.add(pkgServices.EntryDetailResource.class);
         resources.add(pkgServices.EntryResource.class);
+        resources.add(pkgServices.EntryWebResource.class);
         resources.add(pkgServices.LoginResource.class);
         resources.add(pkgServices.PupilResource.class);
         resources.add(pkgServices.TeacherResource.class);
--- PraktikumsverwaltungWebService/src/java/pkgServices/EntryWebResource.java
+++ PraktikumsverwaltungWebService/src/java/pkgServices/EntryWebResource.java
@@ -9,8 +9,8 @@ import com.google.gson.Gson;
 import java.util.ArrayList;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
@@ -23,27 +23,23 @@ import pkgData.Entry;
  *
  * @author schueler
  */
-@Path("EntryAndroid")
-public class EntryAndroidResource {
+@Path("EntryWeb")
+public class EntryWebResource {
 
     @Context
     private UriInfo context;
 
     /**
-     * Creates a new instance of EntryAndroidResource
+     * Creates a new instance of EntryWebResource
      */
-    public EntryAndroidResource() {
+    public EntryWebResource() {
     }
 
-    /**
-     * Retrieves representation of an instance of pkgServices.EntryAndroidResource
-     * @return an instance of java.lang.String
-     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public ArrayList<Entry> getAllEntries() {
+    public String getAllEntries() {
         ArrayList<Entry> listEntries;
-        try { 
+        try {
             Database db = Database.newInstance();
             listEntries = db.getAllEntries();
         }
@@ -52,11 +48,6 @@ public class EntryAndroidResource {
             listEntries.add(new Entry("", null, null, 0.0, ex.getMessage(), "", false, false, false, "", "", ""));
         }
         
-        return listEntries;
-    }
-    
-    @PUT
-    @Consumes(MediaType.APPLICATION_XML)
-    public void putXml(String content) {
+        return new Gson().toJson(listEntries);
     }
 }
